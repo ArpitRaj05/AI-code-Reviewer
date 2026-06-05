1b37de4c289cf4bd43c94cc1374be3c3ac097457
ArpitRaj05 <arpitraj619@gmail.com>
2026-06-05 17:19:03 +0530

    Implement POST /api/review endpoint

--- server/server.js
+++ server/server.js
@@ -2,9 +2,28 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/",(req, res)=>{
     res.send("Server running");
 });
+
+app.post("/api/review", (req,res)=>{
+
+    console.log(req.body);
+
+    const code = req.body.code;
+    const language = req.body.language;
+
+    console.log({
+        code,
+        language
+    })
+
+    res.json({
+        review: "Your code looks fine !!"
+    });
+})
 
 app.listen(5000, ()=>{
     console.log("Server started on port 5000");
